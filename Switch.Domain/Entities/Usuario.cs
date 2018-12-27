f9472a6df2a8d0231ac5eb5cc3368868f89e62a6
Nelson Junior <3nelsonjunior@gmail.com>
2018-12-27 21:22:33 -0200

    2.13 - Especificando a primeira Entidade

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Switch.Domain.Enums;
+
+namespace Switch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int id { get; private set; }
+        public string nome { get; set; }
+        public string sobreNome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public SexoEnum sexo { get; set; }
+        public string urlFoto { get; set; }
+    }
+}
