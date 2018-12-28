543f640f56bf5699c19c342192347b67886a04bc
Nelson Junior <3nelsonjunior@gmail.com>
2018-12-28 20:47:32 -0200

    2.17 - Mapeando Primeira Classe

---
+++ Switch.Infra.Data/Migrations/20181228224101_Inicial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(nullable: true),
+                    sobreNome = table.Column<string>(nullable: true),
+                    email = table.Column<string>(nullable: true),
+                    senha = table.Column<string>(nullable: true),
+                    dataNascimento = table.Column<DateTime>(nullable: false),
+                    sexo = table.Column<int>(nullable: false),
+                    urlFoto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
