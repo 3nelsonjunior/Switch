4978ee7158c80dc4e34e568a3ec160bbd2e2d651
Nelson Junior <3nelsonjunior@gmail.com>
2018-12-28 20:12:36 -0200

    Correções de digitação

--- Switch.API/Startup.cs
+++ Switch.API/Startup.cs
@@ -10,8 +10,8 @@ namespace Switch.API
 {
     public class Startup
     {
-        IConfiguration Configuration { get; set; }
-        public Startup()
+        IConfiguration Configuration { get;}
+        public Startup(IConfiguration configuration)
         {
             var builder = new ConfigurationBuilder().AddJsonFile("config.json");
             Configuration = builder.Build();
@@ -27,7 +27,7 @@ namespace Switch.API
             services.AddMvcCore();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
